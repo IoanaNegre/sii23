8e7f9c42c23f135439a3cb251293c0e4c3688606
IoanaNegre <ioana_negre.com>
2022-03-15 14:00:37 -0700

    changes

---
+++ javascript/variabile.js
@@ -0,0 +1,49 @@
+let numar = 1;//definesc variabila numar si asignez valoarea 1
+let numarCuVirgula = 1.5;
+let string = "Ana are mere";
+let string1= 'Ana are mere !';
+let boolTrue = true;
+let boolFalse = false;
+/*
+comentez mai multe linii
+*/
+numar = 1 + 2;
+numar = numar * 2;// numar *= 2; numar += 2; numar -= 2; numar /= 2;
+numar++;//numar = numar + 1;
+numar--;//numar = numar - 1;
+numar = numar % 2;//restul impartirii
+numar = numar ** 3;//numar la puterea 3
+numar =  Math.pow(numar,3);//numar la puterea 3
+let sir = "A" + "B"//un nou sir de caractere "AB"
+sir +="Merge";
+
+//!true => false; !false => true;
+//AND &&
+let x = true && false;
+//OR ||
+let y = true || false;
+
+// ===  egal
+// !== diferit
+let nr = 6
+
+if(nr%2===0){
+  alert("Numarul "+ nr + " este par")
+  `
+  $(numar)
+  
+  `
+} else {
+    alert(`Numarul $(nr) este par`);
+}
+
+function maxnumber(parametrii){
+    //operatii ce trebuie facute
+    return rezultat;
+}
+
+
+
+
+
+
