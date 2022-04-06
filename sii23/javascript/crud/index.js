eeec0812791721211cd020bde609413cf421374b
IoanaNegre <ioana_negre.com>
2022-04-06 11:33:12 -0700

    changes

--- sii23/javascript/crud/index.js
+++ sii23/javascript/crud/index.js
@@ -1,13 +1,34 @@
 let state = {
   list: [
     {
-      title: "Google",
+      title: "google",
       url: "https://google.com",
-      difficulty: "2",
+      difficulty: "3",
       tags: ["JS", "HTML"],
+    },
+    {
+      title: "Google2",
+      url: "https://google.com",
+      difficulty: "1",
+      tags: ["JSasd", "HTML"],
+    },
+    {
+      title: "Google3",
+      url: "https://google.com",
+      difficulty: "4",
+      tags: ["js", "HTML"],
+    },
+    {
+      title: "Google4",
+      url: "https://google.com",
+      difficulty: "0",
+      tags: ["HTML", "JS"],
     },
   ],
   idxEdit: null,
+  sortColumn: null,
+  sortDirection: 1, //1 e asc, -1 e desc
+
   difficulty: {
     "": "",
     0: "Entry Level",
@@ -17,6 +38,65 @@ let state = {
     4: "Hacker",
   },
 };
+
+function sortTable(th, column) {
+  //persoana.nume
+  //persoana["nume"]
+  //persoana[coloana]
+  //persoana["pre"+"nume"]
+  //persoana["varsta"+i]
+  let sortDirectionSpans = document.querySelectorAll(".sortDirection");
+  for (let span of sortDirectionSpans) {
+    span.innerText = "";
+  }
+
+  if (state.sortColumn === column) {
+    state.sortDirection = -state.sortDirection;
+  } else {
+    state.sortDirection = 1;
+  }
+  state.sortColumn = column;
+
+  // if (state.sortDirection === 1) {
+  //   th.querySelector(".sortDirection").innerHTML = "&darr;";
+  // } else {
+  //   th.querySelector(".sortDirection").innerHTML = "&uarr;";
+  // }
+
+  th.querySelector(".sortDirection").innerHTML =
+    state.sortDirection === 1 ? "&darr;" : "&uarr;";
+
+  state.list.sort(function (a, b) {
+    let columnA = a[column];
+    let columnB = b[column];
+    if (columnA instanceof Array) {
+      columnA = columnA.join();
+    }
+    if (columnB instanceof Array) {
+      columnB = columnB.join();
+    }
+    columnA = columnA.toLowerCase();
+    columnB = columnB.toLowerCase();
+
+    if (a[column] < b[column]) {
+      return -1 * state.sortDirection;
+    } else if (a[column] > b[column]) {
+      return 1 * state.sortDirection;
+    } else {
+      return 0;
+    }
+  });
+
+  draw();
+}
+
+//O functie "compare" care primeste 2 parametrii si
+//returneaza -1 daca primul e mai mic ca al doilea, <<returneaza mai mic decat 0>>
+//0 daca sunt egale si
+//1 daca primul e mai mare ca al doilea <<returneaza mai mare decat 0>>
+function compare(a, b) {
+  return a - b;
+}
 
 function draw() {
   let table = document.querySelector("#list tbody");
@@ -36,9 +116,12 @@ function draw() {
         `;
   }
   table.innerHTML = str;
+  showTable();
 }
 
 function edit(idx) {
+  showForm();
+
   let elem = state.list[idx];
   document.querySelector("[name='title']").value = elem.title;
   document.querySelector("[name='url']").value = elem.url;
@@ -54,6 +137,7 @@ function edit(idx) {
   }
   state.idxEdit = idx;
 }
+
 function del(idx) {
   if (
     confirm(`Esti sigur ca vrei sa stergi linkul: ${state.list[idx].title}?`)
@@ -95,15 +179,35 @@ function adauga(event) {
   document.querySelector("form").reset();
   draw();
 }
-function addTag(event) {
-  //pentru ca click pe buton dadea submit la formular;
-  if (event !== undefined && event !== null) {
-    event.preventDefault();
-  }
-
+function addTag() {
   let button = document.querySelector("#addTagBtn");
   button.parentElement.insertAdjacentHTML(
     "beforeend",
     `<br/><input type="text" placeholder="Tag" name="tags" />`
   );
+}
+
+function showTable() {
+  document.querySelector("#list").classList.remove("hidden");
+  document.querySelector("#form").classList.add("hidden");
+}
+function showForm() {
+  document.querySelector("#list").classList.add("hidden");
+  document.querySelector("#form").classList.remove("hidden");
+}
+function resetForm() {
+  document.querySelector("#form").reset();
+  //delete the extra tag inputs
+  let tagInputs = document.querySelectorAll("[name='tags']");
+  for (let i = 1; i < tagInputs.length; i++) {
+    tagInputs[i].remove();
+  }
+  //and all the BRs
+  let brs = tagInputs[0].parentElement.querySelectorAll("br");
+  for (let br of brs) {
+    br.remove();
+  }
+
+  state.idxEdit = null;
+  showForm();
 }
